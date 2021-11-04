3ccc132bbf39d30839aa01530a0703f0803efe2d
SandeSandev <sashogoranov@gmail.com>
2021-11-04 20:31:46 +0200

    done project structure

---
+++ client/src/app/layouts/content_pages_layout/content-pages-layout/content-pages-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-pages-layout',
+  templateUrl: './content-pages-layout.component.html',
+  styleUrls: ['./content-pages-layout.component.scss']
+})
+export class ContentPagesLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
