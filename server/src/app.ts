5823342df934b69398fa4c1fd308a991c54c073d
alexxgoranov <alexxgoranov@gmail.com>
2021-11-11 12:03:49 +0200

    create sign-up user API call

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,6 +9,8 @@ import routes from './routes';
 const port = config.get<number>('port');
 const app = express();
 
+app.use(express.json());
+
 app.listen(port, async () => {
     logger.info(`App is running at http://localhost:${port}`);
     await connect();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
 
 import {Express, Request, Response} from 'express';
+import { createUserHandler } from './controller/user.controller';
+
+import validateResource from './middleware/validateResource';
+import { createUserSchema } from './schemas/user.schema';
 
 function routes(app: Express) {
-    app.get('/firstApiCall', (req: Request, response: Response) => {
+    app.get('/apiTestCall', (req: Request, response: Response) => {
         response.sendStatus(200);
-    } )
+    } );
+
+    app.post('/createNewUser', validateResource(createUserSchema), createUserHandler);
 }
 
 export default routes;
